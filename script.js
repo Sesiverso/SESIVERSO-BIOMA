c7106439ac764ff8b2a7619ef78c687351657d3f
Sesiverso <94802700+Sesiverso@users.noreply.github.com>
2024-08-29 09:03:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+            console.log('Service Worker registrado com sucesso:', registration.scope);
+        }, function(err) {
+            console.log('Falha ao registrar o Service Worker:', err);
+        });
+    });
+}
