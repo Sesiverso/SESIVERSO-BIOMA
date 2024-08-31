7200c199ea4fcf71dbd307927917c9bace5b9641
Adelio Junior <adelio.junior@afinz.com.br>
2024-08-31 08:53:25 -0300

    Otimização

--- forms/contact.php
+++ forms/contact.php
@@ -7,12 +7,12 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'luizguilhermevalentinereis@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Servidor indisponível!');
   }
 
   $contact = new PHP_Email_Form;
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp-relay.brevo.com',
+    'username' => 'adelionep@gmail.com',
+    'password' => 'yAYbphZnd1vGCEST',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
